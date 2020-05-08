1937198c8f36cdf9868d7c2f95fb99e10bcabd57
Rui Coelho <ruicoelho@ua.pt>
2020-05-09 00:17:39 +0100

    loading widget

--- istudy/lib/screens/authenticate/register.dart
+++ istudy/lib/screens/authenticate/register.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:istudy/services/auth.dart';
 import 'package:istudy/widgets/image_banner.dart';
 import 'package:istudy/shared/constants.dart';
+import 'package:istudy/widgets/loading.dart';
 
 class Register extends StatefulWidget {
   final Function toogleView;
@@ -13,6 +14,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _authService = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -20,98 +22,107 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        title: Text('Sign up'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toogleView();
-              },
-              icon: Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
-              label: Text(
-                'Sign In',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ))
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                ImageBanner(assetPath: "assets/images/logo.jpg"),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: formDecoration.copyWith(hintText: 'Email'),
-                  validator: (val) => val.isEmpty || !val.contains('@')
-                      ? 'Enter an email'
-                      : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: formDecoration.copyWith(hintText: 'Password'),
-                  obscureText: true,
-                  validator: (val) =>
-                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                RaisedButton(
-                  color: Colors.grey[100],
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.black,
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Colors.blue[400],
+              elevation: 0.0,
+              title: Text('Sign up'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toogleView();
+                    },
+                    icon: Icon(
+                      Icons.person,
+                      color: Colors.white,
                     ),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result =
-                          await _authService.signUp(email, password);
-                      if (result == null) {
-                        setState(
-                            () => error = 'Please supply a valid combination.');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 12.0,
-                ),
-                Text(
-                  error,
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 14.0,
-                  ),
-                )
+                    label: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ))
               ],
             ),
-          ),
-        ),
-      ),
-    );
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      ImageBanner(assetPath: "assets/images/logo.jpg"),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration: formDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) => val.isEmpty || !val.contains('@')
+                            ? 'Enter an email'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            formDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true,
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      RaisedButton(
+                        color: Colors.grey[100],
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result =
+                                await _authService.signUp(email, password);
+                            if (result == null) {
+                              setState(
+                                () {
+                                  error = 'Please supply a valid combination.';
+                                  loading = false;
+                                },
+                              );
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 14.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 }
--- istudy/lib/screens/authenticate/sign_in.dart
+++ istudy/lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:istudy/services/auth.dart';
 import 'package:istudy/widgets/image_banner.dart';
 import 'package:istudy/shared/constants.dart';
+import 'package:istudy/widgets/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toogleView;
@@ -14,6 +15,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -21,99 +23,106 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        title: Text('Sign in'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toogleView();
-              },
-              icon: Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
-              label: Text(
-                'Register',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ))
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                ImageBanner(assetPath: "assets/images/logo.jpg"),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: formDecoration.copyWith(hintText: 'Email'),
-                  validator: (val) => val.isEmpty || !val.contains('@')
-                      ? 'Enter an email'
-                      : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: formDecoration.copyWith(hintText: 'Password'),
-                  validator: (val) =>
-                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                RaisedButton(
-                  color: Colors.grey[100],
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(
-                      color: Colors.black,
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Colors.blue[400],
+              elevation: 0.0,
+              title: Text('Sign in'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toogleView();
+                    },
+                    icon: Icon(
+                      Icons.person,
+                      color: Colors.white,
                     ),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _authService.signInEmailPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(
-                            () => error = 'Please supply a valid combination.');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 12.0,
-                ),
-                Text(
-                  error,
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 14.0,
-                  ),
-                )
+                    label: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ))
               ],
             ),
-          ),
-        ),
-      ),
-    );
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      ImageBanner(assetPath: "assets/images/logo.jpg"),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration: formDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) => val.isEmpty || !val.contains('@')
+                            ? 'Enter an email'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            formDecoration.copyWith(hintText: 'Password'),
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      RaisedButton(
+                        color: Colors.grey[100],
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _authService
+                                .signInEmailPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error = 'Please supply a valid combination.';
+                                loading = false;
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 14.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 }
 
