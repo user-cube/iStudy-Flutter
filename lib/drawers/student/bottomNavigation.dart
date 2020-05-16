2a1097353bbcfa88012253830d94fd3478f4d57d
Rui Coelho <ruicoelho@ua.pt>
2020-05-16 21:28:30 +0100

    dynamic changes

--- lib/drawers/student/bottomNavigation.dart
+++ lib/drawers/student/bottomNavigation.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:istudy/app.dart';
 
-class BottomNavigatorBar extends StatelessWidget {
+class BottomNavigatorBar extends StatefulWidget {
+  @override
+  _BottomNavigatorBarState createState() => _BottomNavigatorBarState();
+}
+
+class _BottomNavigatorBarState extends State<BottomNavigatorBar> {
+  int myIndex = 0;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: myIndex,
       onTap: (int index) {
         print(index);
         switch (index) {
           case 0:
+            setState(() => myIndex = 0);
             Navigator.pushNamed(context, StudentHomeRoute);
             break;
           case 1:
+            setState(() => myIndex = 1);
             Navigator.pushNamed(context, AddNotesRoute);
             break;
           case 2:
-            //
+            setState(() => myIndex = 2);
             break;
           default:
             break;
